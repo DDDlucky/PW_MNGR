e6db87d9a58c28a99c7c551f6e9f3933d4f46d73
DDDlucky <d.ono2105@gmail.com>
2024-02-06 13:27:42 +0900

    step1 finish

---
+++ step1.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+echo "パスワードマネージャーへようこそ！"
+echo "サービス名を入力してください："
+read serviceName
+
+echo "ユーザー名を入力してください："
+read userName
+
+echo "パスワードを入力してください："
+read passWord
+
+echo "$serviceName:$userName:$passWord" >> "password.txt"
+
+# 入力が完了したら
+echo "Thank you!"
